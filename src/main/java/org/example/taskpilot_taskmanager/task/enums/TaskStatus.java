ccf1d3f36ccff4122c94fc3e23b50c2b23323a6f
YashR-github <71266760+YashR-github@users.noreply.github.com>
2026-03-18 13:58:53 +0530

    Apply minor refactoring in TaskStatus

--- src/main/java/org/example/taskpilot_taskmanager/task/enums/TaskStatus.java
+++ src/main/java/org/example/taskpilot_taskmanager/task/enums/TaskStatus.java
@@ -2,9 +2,8 @@ package org.example.taskpilot_taskmanager.task.enums;
 
 public enum TaskStatus {
     NOT_STARTED,
-    INPROGRESS, // might change the status to  inprogress when started on a task
+    INPROGRESS, // might change the status to  in progress when started on a task
     COMPLETED,
-
     //future features
     RESCHEDULED, // might help to show to user the date it is rescheduled to
     ONHOLD,  // might provide option to put task on hold, // might also allow reminder for this task to start
